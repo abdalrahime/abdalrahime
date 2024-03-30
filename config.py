c3708c4228b4976f43c79f438bfc1e88d0def2b2
abdalrahime <165474309+abdalrahime@users.noreply.github.com>
2024-03-30 13:10:18 +0200

    config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-
+class Config:
+# Telegram API settings
+API_ID = “6815957”
+API_HASH = “e6c7d4898439784c379884df12335137”
+BOT_TOKEN = “5271794343:AAExO18b5tnOiaKo1j3u_Ym2Lokd3nk3j-o”
+# OpenAI API settings
+OPENAI_API_KEY = “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”
